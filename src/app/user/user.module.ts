9f1dbb92de0144cee8a419420e56b12703301e0d
larionovdmitri <dlarionov@productiveedge.com>
2019-03-26 18:28:22 -0500

    saved components

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core'
 import { CommonModule } from '@angular/common'
 import { RouterModule } from '@angular/router'
 
+
 //component routes
 import { userRoutes } from './user.routes'
 
